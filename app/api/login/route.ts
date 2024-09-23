a129cc046c9e4872b5da7d58813673e848ccd5f8
psyger100 <rvsng028@gmail.com>
2024-09-23 20:51:35 +0530

    login, signup feature with middleware

---
+++ app/api/login/route.ts
@@ -0,0 +1,44 @@
+import { userTable } from "@/utils/prisma";
+import { generateAccessToken, generateRefreshToken } from "@/utils/Tokens";
+import argon2 from "argon2";
+
+export async function POST(request: Request) {
+    const { email, password } = await request.json();
+
+    try {
+        const user = await userTable.findUnique({
+            where: {
+                email: email,
+            },
+        });
+        console.log(user);
+
+        if (!user) {
+            return Response.json({ message: "User not found" }, { status: 404 });
+        }
+        const passwordMatch = await argon2.verify(user.password, password);
+        if (!passwordMatch) {
+            return Response.json({ message: "Invalid password" }, { status: 401 });
+        }
+
+        const accessToken = await generateAccessToken(user);
+        const refreshToken = await generateRefreshToken(user);
+        await userTable.update({
+            where: {
+                id: user.id,
+            },
+            data: {
+                refreshToken: refreshToken,
+            },
+        });
+
+        return Response.json({
+            setAccessToken: accessToken,
+            setRefreshToken: refreshToken,
+        });
+    } catch (error: any) {
+        console.log("I am the error :- > ", error.message, "\n\n\n");
+
+        return Response.json({ message: "Login Failed" });
+    }
+}
---
+++ middleware.ts
@@ -0,0 +1,48 @@
+import { NextRequest, NextResponse } from "next/server";
+import { userTable } from "./utils/prisma";
+import { generateAccessToken } from "./utils/Tokens";
+import jwt from "jsonwebtoken";
+import * as jose from "jose";
+export async function middleware(request: NextRequest, response: NextResponse) {
+  const accessToken = request.headers.get("accessToken");
+  const refreshToken = request.headers.get("refreshToken");
+  if (!accessToken && !refreshToken) {
+    return NextResponse.json({ message: "Login Required!!!" }, { status: 401 });
+  } else {
+    try {
+      try {
+        const decoded_value: any = await jose.jwtVerify(
+          accessToken?.toString() as string,
+          new TextEncoder().encode(process.env.ACCESS_TOKEN_SECRET as string)
+        );
+
+        // const accessTokenDbVerification = await userTable.findUnique({
+        //   where: {
+        //     id: decoded_value.payload.id,
+        //   },
+        // });
+        // if (refreshToken === accessTokenDbVerification?.refreshToken) {
+        //   delete (decoded_value as { iat?: number })?.iat;
+        //   delete (decoded_value as { exp?: number })?.exp;
+        //   request.headers.set("current_user", JSON.stringify(decoded_value));
+        //   console.log("Next function called");
+
+        //   return NextResponse.next({
+        //     request: request,
+        //   });
+        // }
+      } catch (error: any) {
+        console.log(
+          "\n\n\n\n\n\n\n\n\n\n\n\n",
+          error.message,
+          "\n\n\n\n\n\n\n\n\n\n\n\n"
+        );
+      }
+    } catch (error: any) {
+      return NextResponse.json({ message: "Login Required" }, { status: 401 });
+    }
+  }
+}
+export const config = {
+  matcher: "/api/sayHello",
+};
