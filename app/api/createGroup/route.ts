1681bbc250cfdaad91a094a1009bec6d937796d5
psyger100 <rvsng028@gmail.com>
2024-09-26 18:37:33 +0530

    Refactor createGroup route to handle group creation and membership

---
+++ app/api/createGroup/route.ts
@@ -0,0 +1,69 @@
+import { groupTable, userOnGroupsTable } from "@/utils/prisma";
+
+export async function POST(request: Request) {
+    try {
+        let info: {
+            createGroup: {
+                name: string;
+                adminBudget: Number | string;
+                members: {
+                    id: String;
+                    maxAmount: String | number;
+                    restAmount: String | number;
+                }[];
+            };
+        } = await request.json();
+
+        const current_user = request.headers.get("current_user");
+
+        console.log("\n\n\n\n", current_user, "\n\n\n\n");
+
+        const current_user_id = JSON.parse(current_user as string).payload.id;
+        const groupCreation = await groupTable.create({
+            data: {
+                name: info.createGroup.name,
+            },
+        });
+        if (groupCreation) {
+            console.log(groupCreation);
+
+            const users = info.createGroup.members.map(
+                (item: {
+                    id: String;
+                    maxAmount: String | number;
+                    restAmount: String | number;
+                }) => ({
+                    groupId: groupCreation.id,
+                    userId: item.id,
+                    maxAmount: +item.maxAmount,
+                    restAmount: +item.restAmount,
+                }),
+            );
+            users.push({
+                groupId: groupCreation.id,
+                userId: current_user_id,
+                maxAmount: +info.createGroup.adminBudget,
+                restAmount: +info.createGroup.adminBudget,
+            });
+
+            if (users.length > 0) {
+                const userOnGroups = await userOnGroupsTable.createMany({
+                    // @ts-ignore
+                    data: users,
+                });
+                if (userOnGroups) {
+                    return Response.json(
+                        { message: "Group created successfully" },
+                        { status: 200 },
+                    );
+                }
+            }
+        }
+        return Response.json({ message: "Error creating group" }, { status: 500 });
+    } catch (error) {
+        // @ts-ignore
+        console.log(error.message);
+
+        return Response.json({ message: "Error creating group" }, { status: 500 });
+    }
+}
---
+++ app/api/getFriends/route.ts
@@ -0,0 +1,29 @@
+import { friendTable } from "@/utils/prisma";
+
+export async function GET(request: Request) {
+    try {
+        const current_user = request.headers.get("current_user");
+        const current_user_id = JSON.parse(current_user as string).payload.id;
+        const data = await friendTable.findMany({
+            where: {
+                userId: current_user_id,
+                isAccepted: true,
+            },
+            select: {
+                friend: {
+                    select: {
+                        name: true,
+                        email: true,
+                        id: true,
+                    },
+                },
+            },
+        });
+        if (data) {
+            return Response.json(data, { status: 200 });
+        }
+        return Response.json({ message: "Error getting friends" }, { status: 500 });
+    } catch (error) {
+        return Response.json({ message: "Error getting friends" }, { status: 500 });
+    }
+}
