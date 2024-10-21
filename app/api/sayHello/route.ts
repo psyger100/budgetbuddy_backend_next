461c989d26b53c25a901e16912c56e59d421a488
psyger100 <rvsng028@gmail.com>
2024-10-21 20:25:06 +0530

    update:- sayHello rollback

--- app/api/sayHello/route.ts
+++ app/api/sayHello/route.ts
@@ -1,9 +1,3 @@
 export async function GET() {
-    const headers = new Headers({
-        "Access-Control-Allow-Origin": "*", // Allow all origins
-        "Access-Control-Allow-Methods": "GET, POST, OPTIONS", // Allow specific methods
-        "Access-Control-Allow-Headers": "Content-Type", // Allow specific headers
-    });
-
-    return Response.json("Hello world", { status: 200, headers });
+    return Response.json("Hello world", { status: 200 });
 }
