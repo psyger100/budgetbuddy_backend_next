ba3a0c58e2088fdb8141f3086f12a468fdc6882f
psyger100 <rvsng028@gmail.com>
2024-10-21 23:15:36 +0530

    update: synced info with database

--- app/api/whoAmI/route.tsx
+++ app/api/whoAmI/route.tsx
@@ -1,3 +1,4 @@
+import { userTable } from "@/utils/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 interface currentUserType {
@@ -12,8 +13,22 @@ interface currentUserType {
 export async function GET(request: NextRequest) {
     const currentUserHeader: any = request.headers.get("current_user");
     try {
+        const new_info = await userTable.findUnique({
+            where: {
+                id: JSON.parse(currentUserHeader).payload.id,
+            },
+        });
+
         return Response.json(
-            { userInformation: JSON.parse(currentUserHeader) },
+            {
+                userInformation: {
+                    id: new_info?.id,
+                    userName: new_info?.userName,
+                    name: new_info?.name,
+                    email: new_info?.email,
+                    avatar_url: new_info?.avatar_url,
+                },
+            },
             { status: 200 },
         );
     } catch (error: any) {
